7ef7465f9ff85ad8884e207441ee55711b24136f
pradeep_dawaljee <pradeep.dawaljee@raksan.in>
2018-03-18 13:21:36 +0530

    Added Code For Zip File

--- src/main/java/com/framework/Package/core/BaseClass.java
+++ src/main/java/com/framework/Package/core/BaseClass.java
@@ -1,5 +1,6 @@
 package com.framework.Package.core;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
@@ -8,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
@@ -15,6 +17,7 @@ import com.framework.Package.helper.LoggerHelper;
 import com.framework.Package.reader.ExcelClass;
 import com.framework.Package.reader.PropertyFileReader;
 import com.framework.Package.report.ExtentReport;
+import com.framework.Package.utilities.ZIPFileUtility;
 //import com.framework.Package.reader.PropertyFileReader;
 
 public class BaseClass extends ExtentReport {
@@ -22,6 +25,7 @@ public class BaseClass extends ExtentReport {
 	public static WebDriver driver;
 	public static ExcelClass data;
 	private Logger log=LoggerHelper.getLogger(BaseClass.class);
+	ZIPFileUtility zipFileUtility;
 
 	@BeforeClass
 	@Parameters({"browserType","environment"})
@@ -81,6 +85,15 @@ public class BaseClass extends ExtentReport {
 		extent.flush();
 		//extent.close();
 		
+	}
+	
+	@AfterSuite
+	public void tearDown(){
+		zipFileUtility=new ZIPFileUtility();
+		File dir=new File(System.getProperty("user.dir")+"/ScreenShots");
+		String zipDirName="ScreenShots.zip";
+		zipFileUtility.zipDirectory(dir, zipDirName);
+		log.info("Zipping "+zipDirName+" completed successfully");
 	}
 
 }
